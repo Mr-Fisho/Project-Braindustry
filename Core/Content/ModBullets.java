d27ad1bb8d2cea5cb6f9e659cd72b39973ea0269
Mr-Fisho <156609453+Mr-Fisho@users.noreply.github.com>
2024-01-24 20:36:08 +0400

    Create ModBullets.java

---
+++ Core/Content/ModBullets.java
@@ -0,0 +1,28 @@
+package braindustry.content;
+
+import braindustry.entities.bullets.AngelContinuousBulletType;
+import braindustry.entities.bullets.GasBulletType;
+import mindustry.ctype.ContentList;
+import mindustry.entities.bullet.BulletType;
+
+public class ModBullets implements ContentList {
+    public static BulletType deathLaser,
+    //gas
+    heavyMethaneShot;
+
+    @Override
+    public void load() {
+        deathLaser = new AngelContinuousBulletType() {{
+        }};
+
+        heavyMethaneShot = new GasBulletType(ModGasses.methane) {{
+            lifetime = 49f;
+            speed = 4f;
+            puddleSize = 8f;
+            orbSize = 4f;
+            drag = 0.001f;
+            ammoMultiplier = 0.4f;
+            statusDuration = 60f * 4f;
+        }};
+    }
+}
